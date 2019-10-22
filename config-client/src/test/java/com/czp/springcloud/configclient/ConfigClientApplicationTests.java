df65c239caaa921db32bb6e99df18d51baae718e
czp <3056604571@qq.com>
2019-10-22 21:50:04 +0800

    Initial commit

---
+++ config-client/src/test/java/com/czp/springcloud/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czp.springcloud.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
